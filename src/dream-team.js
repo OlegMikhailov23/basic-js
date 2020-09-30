52fb2ee4f5488abec9d6f347be940ccff2a04b84
Oleg Mikhaylov <dkfolkin@gmail.com>
2020-09-30 15:29:51 +0300

    feat: finish dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    let firstLatterCollection = [];
+    members.forEach((it) => {
+      if (typeof it === 'string') {
+        it = it.replace(/\s+/g, '')
+        firstLatterCollection.push(it[0].toUpperCase());
+      }
+    });
+
+    return firstLatterCollection.sort().join('').trim();
+  } else {
+
+    return false
+  }
 };
