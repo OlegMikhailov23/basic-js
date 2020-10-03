5f6062b8df5394a6d883b1e25eb96edc7e68e8df
Oleg Mikhaylov <dkfolkin@gmail.com>
2020-10-03 23:10:14 +0300

    feat: have only 2 problrm with tranform array:)

--- src/index.js
+++ src/index.js
@@ -1,24 +1,32 @@
-let myArr = [ 1, 2, 3, '--double-next', 4, 5 ,'--double-next'];
+let myArr = [1, 2, 3, '--discard-next', 1337, '--discard-prev', 4, 5];
 
 const findIndex = (arr) => {
     let arrCopy = arr;
-    for (let i = 0; i <= arrCopy.length; i++) {
+    for (let i = 0; i < arrCopy.length; i++) {
+
         if (arrCopy[i] === `--discard-next`) {
+            i = 0;
             arrCopy.splice(arrCopy.indexOf('--discard-next'), 2);
+
         } else if (arrCopy[i] === `--discard-prev`) {
+            i = 0
             if (arrCopy.indexOf('--discard-prev') === 0) {
                 arrCopy.splice(arrCopy.indexOf('--discard-prev'), 1)
             } else {
                 arrCopy.splice((arrCopy.indexOf('--discard-prev')) -1, 2);
             }
+
         } else if (arrCopy[i] === `--double-prev`) {
+            i = 0
             if (arrCopy.indexOf('--double-prev') === 0) {
                 arrCopy.splice(arrCopy.indexOf('--double-prev'), 1)
             } else {
                 arrCopy[arrCopy.indexOf('--double-prev')] = arrCopy[(arrCopy.indexOf('--double-prev')) -1];
                 arrCopy.splice(arrCopy.indexOf('--double-prev'), 0);
             }
+
         } else if (arrCopy[i] === `--double-next`) {
+            i = 0
             if (arrCopy.indexOf('--double-next') === arrCopy.length -1) {
                 arrCopy.splice(arrCopy.indexOf('--double-next'), 1);
             } else {
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,17 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-    let arrCopy = arr;
-    for (let i = 0; i <= arrCopy.length; i++) {
+    let arrCopy = arr.slice();
+    for (let i = 0; i < arrCopy.length; i++) {
         if (arrCopy[i] === `--discard-next`) {
+            i = 0;
             arrCopy.splice(arrCopy.indexOf('--discard-next'), 2);
         } else if (arrCopy[i] === `--discard-prev`) {
+            i = 0
             if (arrCopy.indexOf('--discard-prev') === 0) {
                 arrCopy.splice(arrCopy.indexOf('--discard-prev'), 1)
             } else {
                 arrCopy.splice((arrCopy.indexOf('--discard-prev')) -1, 2);
             }
         } else if (arrCopy[i] === `--double-prev`) {
+            i = 0
             if (arrCopy.indexOf('--double-prev') === 0) {
                 arrCopy.splice(arrCopy.indexOf('--double-prev'), 1)
             } else {
@@ -19,6 +22,7 @@ module.exports = function transform(arr) {
                 arrCopy.splice(arrCopy.indexOf('--double-prev'), 0);
             }
         } else if (arrCopy[i] === `--double-next`) {
+            i = 0
             if (arrCopy.indexOf('--double-next') === arrCopy.length -1) {
                 arrCopy.splice(arrCopy.indexOf('--double-next'), 1);
             } else {
