60489ead9d146b9bfad633f98ec0231f6a96f70f
Oleg Mikhaylov <dkfolkin@gmail.com>
2020-10-04 22:22:43 +0300

    feat: transform arrays resolved

--- src/transform-array.js
+++ src/transform-array.js
@@ -5,18 +5,20 @@ module.exports = function transform(arr) {
     for (let i = 0; i < arrCopy.length; i++) {
         if (arrCopy[i] === `--discard-next`) {
             i = 0;
-            arrCopy.splice(arrCopy.indexOf('--discard-next'), 2);
+            arrCopy[(arrCopy.indexOf('--discard-next')) + 1] = undefined;
+            arrCopy[arrCopy.indexOf('--discard-next')] = undefined;
         } else if (arrCopy[i] === `--discard-prev`) {
             i = 0
             if (arrCopy.indexOf('--discard-prev') === 0) {
-                arrCopy.splice(arrCopy.indexOf('--discard-prev'), 1)
+                arrCopy.shift();
             } else {
-                arrCopy.splice((arrCopy.indexOf('--discard-prev')) -1, 2);
+                arrCopy[(arrCopy.indexOf('--discard-prev')) - 1] = undefined;
+                arrCopy[arrCopy.indexOf('--discard-prev')] = undefined;
             }
         } else if (arrCopy[i] === `--double-prev`) {
             i = 0
             if (arrCopy.indexOf('--double-prev') === 0) {
-                arrCopy.splice(arrCopy.indexOf('--double-prev'), 1)
+                arrCopy.shift();
             } else {
                 arrCopy[arrCopy.indexOf('--double-prev')] = arrCopy[(arrCopy.indexOf('--double-prev')) -1];
                 arrCopy.splice(arrCopy.indexOf('--double-prev'), 0);
@@ -24,11 +26,12 @@ module.exports = function transform(arr) {
         } else if (arrCopy[i] === `--double-next`) {
             i = 0
             if (arrCopy.indexOf('--double-next') === arrCopy.length -1) {
-                arrCopy.splice(arrCopy.indexOf('--double-next'), 1);
+                arrCopy.pop();
             } else {
                 arrCopy[arrCopy.indexOf('--double-next')] = arrCopy[(arrCopy.indexOf('--double-next')) + 1];
             }
         }
     }
-    return arrCopy
+
+    return arrCopy.filter(it => it !== undefined);
 };
