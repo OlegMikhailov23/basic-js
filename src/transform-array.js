82017936ddc20327e9265ca76b0d9d837692072d
Oleg Mikhaylov <dkfolkin@gmail.com>
2020-10-04 22:40:26 +0300

    feat: transform arrays resolved

--- src/transform-array.js
+++ src/transform-array.js
@@ -4,11 +4,9 @@ module.exports = function transform(arr) {
     let arrCopy = arr.slice();
     for (let i = 0; i < arrCopy.length; i++) {
         if (arrCopy[i] === `--discard-next`) {
-            i = 0;
             arrCopy[(arrCopy.indexOf('--discard-next')) + 1] = undefined;
             arrCopy[arrCopy.indexOf('--discard-next')] = undefined;
         } else if (arrCopy[i] === `--discard-prev`) {
-            i = 0
             if (arrCopy.indexOf('--discard-prev') === 0) {
                 arrCopy.shift();
             } else {
@@ -16,7 +14,6 @@ module.exports = function transform(arr) {
                 arrCopy[arrCopy.indexOf('--discard-prev')] = undefined;
             }
         } else if (arrCopy[i] === `--double-prev`) {
-            i = 0
             if (arrCopy.indexOf('--double-prev') === 0) {
                 arrCopy.shift();
             } else {
@@ -24,7 +21,6 @@ module.exports = function transform(arr) {
                 arrCopy.splice(arrCopy.indexOf('--double-prev'), 0);
             }
         } else if (arrCopy[i] === `--double-next`) {
-            i = 0
             if (arrCopy.indexOf('--double-next') === arrCopy.length -1) {
                 arrCopy.pop();
             } else {
