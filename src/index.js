17df4d9349b36035e424a93676251831126e3eae
Oleg Mikhaylov <dkfolkin@gmail.com>
2020-10-06 01:10:52 +0300

    feat: simpl-chain resolved

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-const CustomError = require("../extensions/custom-error");
-
 const chainMaker = {
     res: [],
     getLength() {
+        console.log(this.res.length)
         return this.res.length
     },
     addLink(value) {
@@ -27,10 +26,11 @@ const chainMaker = {
     },
     finishChain() {
         let resCopy = this.res.slice();
+        console.log(resCopy, resCopy.join('~~'));
         this.res = [];
         return resCopy.join('~~');
     }
+
 };
 
-
-module.exports = chainMaker;
+chainMaker.addLink('8.963').reverseChain().reverseChain().reverseChain().reverseChain().addLink({0: 'first', 1: 'second', 'length': 2}).reverseChain().addLink(3.14).addLink('DEF').reverseChain().finishChain()
