ea4eb6553ad4f3c214661de1794ef607ff6aa873
Oleg Mikhaylov <dkfolkin@gmail.com>
2020-10-30 15:25:00 +0300

    feat: extended repeater done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -3,28 +3,34 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function repeater(str, options) {
     let tempArr = [];
     let tempStr = '';
+
     !options.separator ? options.separator = '+' : '';
+
     if (options.repeatTimes === undefined) {
         options.repeatTimes = 1;
         options.additionSeparator = '';
     }
-    // !options.addition ? options.additionSeparator = '|' : '';
-    // if (options.addition && !options.additionSeparator) {
-    //     options.additionSeparator = '|';
-    // }
-    console.log(options.repeatTimes);
-    for (let i = 0; i < options.repeatTimes; i++) {
 
-        if (options.additionRepeatTimes < 0) {
-            options.additionSeparator = '+';
-            options.addition = '';
+    if (typeof str !== 'string' && typeof options.addition !== 'string') {
+        str = String(str) + '';
+        options.addition = String(options.addition) + '';
+    }
+
+    for (let i = 0; i < options.repeatTimes; i++) {
+        i === options.repeatTimes - 1 ? options.separator = '' : ''
+        !options.additionRepeatTimes ? options.additionRepeatTimes = 1 : '';
+        let aditionAndSep = '';
+        for (let j = 0; j <= options.additionRepeatTimes - 1; j++) {
+            if (j === options.additionRepeatTimes -1) {
+                aditionAndSep += (options.addition ? options.addition : '')
+            } else {
+                aditionAndSep += (options.addition ? options.addition : '') + (options.additionSeparator ? options.additionSeparator : '')
+            }
         }
-        if (i === options.repeatTimes - 1) {
-            options.separator = '';
-        }
-        tempStr = str + (options.addition ? options.addition : '') + (options.additionSeparator ? options.additionSeparator : '') + (options.separator ? options.separator : '');
+        tempStr = str + aditionAndSep + (options.separator ? options.separator : '');
         tempArr.push(tempStr);
     }
-    console.log(tempArr.join(''))
+
+
     return tempArr.join('')
 };
